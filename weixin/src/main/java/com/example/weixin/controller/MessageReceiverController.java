7268ff83f0fb53170263f01acc658aa442079365
hua <hua@DESKTOP-0SEGRMV>
2019-07-01 02:43:51 +0800

    更新代码

--- weixin/src/main/java/com/example/weixin/controller/MessageReceiverController.java
+++ weixin/src/main/java/com/example/weixin/controller/MessageReceiverController.java
@@ -26,7 +26,7 @@ import com.example.weixin.service.MessageTypeMapper;
 @RestController
 // 各自写代码的时候，把/kemao_1改为【/拼音名】，用于后面作为路径反向代理的时候区分不同人的代码
 // @RequestMapping表示的含义：URL跟控制器的关系映射
-@RequestMapping("/lzh_1/weixin/receiver")
+@RequestMapping("/lzh/weixin/receiver")
 public class MessageReceiverController {
 
 	private static final Logger LOG = LoggerFactory.getLogger(MessageReceiverController.class);
